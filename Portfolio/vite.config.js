7863583573e51deab75cd3e9dfd127c96c2bd6e3
Shaofu Wang <wang@ShaofutekiMacBook-Air.local>
2026-04-18 01:43:20 +0800

    Updates

--- Portfolio/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // 关键：使用自定义域名时，base 必须是 '/'
-})
